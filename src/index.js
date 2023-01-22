cbaa843e050e9735c853b0f254ad4cf556693066
Mikhail Yarancev <mihaalich2@mail.ru>
2023-01-22 17:13:58 +0300

    add postCss

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import {sum} from './modules/sum';
+import styles from './index.css'
 
 const root = document.querySelector('#root');
 root.textContent = sum(1, -1).toString();
+root.classList.add(styles.app)
