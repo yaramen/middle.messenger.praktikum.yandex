58c49d870b50eb032814071e77efae2faebd10c9
Mikhail Yarancev <mihaalich2@mail.ru>
2023-02-06 23:35:34 +0300

    fix type in store

--- src/modules/store/Store.ts
+++ src/modules/store/Store.ts
@@ -1,15 +1,16 @@
-class Store extends EventTarget {
-    private handlers;
+class Store<T> extends EventTarget {
+    private handlers: ((oldState: T, newState: T) => void)[];
 
-    private state;
+    private state: T;
 
-    constructor(initState) {
+    constructor(initState: T) {
         super();
         this.state = initState;
         this.handlers = [];
     }
 
-    dispatch(...args) {
+    // @ts-ignore
+    dispatch(...args: unknown) {
         // @ts-ignore
         this.dispatchEvent(...args);
     }
@@ -18,18 +19,18 @@ class Store extends EventTarget {
         return this.state;
     }
 
-    setState(func) {
+    setState(func: (state: T) => T) {
         const newState = func(this.state);
         this.handlers.map((handle) => handle(this.state, newState));
         this.state = newState;
     }
 
-    subscribe(handler) {
+    subscribe(handler: (oldState: T, newState: T) => void) {
         this.handlers.push(handler);
         return handler;
     }
 
-    unsubscribe(handler) {
+    unsubscribe(handler: (oldState: T, newState: T) => void) {
         this.handlers = this.handlers.filter((currentHandler) => currentHandler !== handler);
     }
 
